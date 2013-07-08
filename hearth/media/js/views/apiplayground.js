e9008a4a46bd8e8e226ecd5a201115b30aaedf3b
Ashish Dubey <ashish.dubey91@gmail.com>
2013-07-08 14:31:43 -0700

    enable endpoint selections

--- hearth/media/js/views/apiplayground.js
+++ hearth/media/js/views/apiplayground.js
@@ -1,7 +1,16 @@
 define('views/apiplayground',
-    [],
-    function() {
+    ['z'],
+    function(z) {
     'use strict';
+
+   	z.page.on('click', '.api-endpoint', function(e) {
+   		var $this = $(this);
+   		$('.options').hide();
+
+   		$this.parent().siblings('.options').show();
+   		$('.endpoint-container').removeClass('focused');
+   		$this.parent().parent().addClass('focused');
+   	});
 
     return function(builder) {
     	builder.start('playground.html');
