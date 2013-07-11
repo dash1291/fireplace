ce73bc5cf0c54c15f50f9e5e2459e01be6ddb4f2
Ashish Dubey <ashish.dubey91@gmail.com>
2013-07-11 16:40:34 -0700

    pep up the frontend a little

--- hearth/media/js/views/apiplayground.js
+++ hearth/media/js/views/apiplayground.js
@@ -21,42 +21,71 @@ define('views/apiplayground',
     z.page.on('click', '.api-endpoint', function(e) {
         var $this = $(this);
         $('.options').hide();
-
-        $this.parent().siblings('.options').show();
-        $('.endpoint-container').removeClass('focused');
-        $this.parent().parent().addClass('focused');
+        var html = $this.parent().siblings('.options').html();
+        var left = $this[0].offsetLeft;
+        var top = $this[0].offsetTop;
+        var width = $this[0].offsetWidth;
+        //$this.parent().siblings('.options').show();
+        $('.playfield').html(html);
     });
 
     z.page.on('click', '.api-name', function() {
         $('.endpoints-container').addClass('hidden');
-        $(this).parent().siblings('.endpoints-container').removeClass('hidden');
+        $(this).closest('.api-container').find('.endpoints-container')
+                                         .removeClass('hidden');
+    });
+
+    z.page.on('click', 'input[name="view-selector"]', function() {
+        var $this = $(this);
+        var view_name = $this.val();
+        $this.parent().siblings('.view').hide();
+        $this.parent().siblings('.' + view_name + '-view').show();
     });
 
     z.page.on('click', '.send-request', function() {
         var $this = $(this);
-        var url = $this.parent().parent().data('url');
-        var method = $this.parent().parent().data('method');
+        var url = $this.data('url');
+        var method = $this.data('method');
 
-        if (method == 'get') {
-            var params = {};
-            $this.siblings('.request-params').find('input').each(function() {
-                var $_this = $(this);
-                params[$_this.data('name')] = $_this.val();
-            });
+        var params = {};
+        $this.parent().siblings('.request-view').find('.request-params input').each(function() {
+            var $_this = $(this);
+            var val = $_this.val();
+            if (val.length > 0) {
+                params[$_this.data('name')] = val;
+            }
+        });
 
-            url = urls.absolutifyApiUrl(url, params);
-            console.log(url);
-            var args = [];
-            $this.siblings('.url-args').find('input').each(function() {
-                var $_this = $(this);
-                args.push($_this.val());
-            });
+        var args = [];
+        $this.parent().siblings('.request-view').find('.url-args input').each(function() {
+            var $_this = $(this);
+            args.push($_this.val());
+        });
 
-            url = buildUrl(url, args, params);
-            requests.get(url).done(function(data) {
-                console.log($this);
-                $this.siblings('.response').html(JSON.stringify(data, null, '  '));
-            });
+        function errorHandler(err) {
+            var json = JSON.stringify(err.responseText, null, '  ');
+            $this.siblings('.response-view').html('<div><p class="error">Error occured</p><pre>' + json + '</pre></div>');
+        }
+
+        function responseHandler(data) {
+            // Display response in to the response panel.
+            $this.parent().siblings('.response-view').html('<pre>' + JSON.stringify(data, null, '  ') + '</pre>');
+            $this.siblings('.response-selector').trigger('click');
+        }
+
+        switch (method) {
+            case 'get':
+                url = buildUrl(url, args, params);
+                url = urls.absolutifyApiUrl(url, params);
+                requests.get(url).done(responseHandler).fail(errorHandler);
+                break;
+
+            case 'post':
+                // TODO: pass params to the absolutify
+                url = urls.absolutifyApiUrl(url);
+                url = buildUrl(url, args, params);
+                requests.post(url).done(responseHandler).fail(errorHandler);
+                break;
         }
     });
 
